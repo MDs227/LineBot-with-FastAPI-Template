2c1ef394248726bbfad295122d83511151cbaa47
MDs227 <126652028+MDs227@users.noreply.github.com>
2025-03-05 16:24:40 +0800

    Update config.py

--- project/config.py
+++ project/config.py
@@ -6,5 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # LINE Bot 設定
-LINE_CHANNEL_SECRET = os.environ.get("LINE_CHANNEL_SECRET")
-LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.environ.get("83b2f1d02765902bddc2d3c005b48329")
+LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("DYuAmKRAkBRqRMBMxW5iy8aUaSiHeJsfKm3LRArxbSglCwhqS26PKomRAOrUXNUqCtWGSVUe4yWS85eMed2C6PyU6sh0rahw60YODevrg/1eJlNo5sTTTCmBNLp+W29ANIGhGVStHbPcf74loCOGDgdB04t89/1O/w1cDnyilFU=")
+GOOGLE_API_KEY = os.environ.get('AIzaSyDO0bMFUJAH0uD20ZoByUizv3JxZt17VAE')
+FIREBASE_URL = os.environ.get('https://console.firebase.google.com/u/0/project/md227-c608e/database/md227-c608e-default-rtdb/data/~2F')
